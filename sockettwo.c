2632ae64f062ec3437430529cf19b3300d375a4e
intan <intanamira2001@yahoo.com>
2022-06-22 22:46:28 +0800

    sockettwo

--- sockettwo.c
+++ sockettwo.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<string.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>   //inet_addr
 
@@ -5,6 +7,7 @@ int main(int argc, char *argv[])
 {
 int socket_desc;
 struct sockaddr_in server;
+char *message;
 
 //create socket
 socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -13,7 +16,7 @@ if (socket_desc == -1)
 printf("Could not create socket");
 }
 
-server.sin_addr.s_addr = inet_addr("192.168.56.106"); //Please enter the ip address of your Server VM
+server.sin_addr.s_addr = inet_addr("192.168.56.106"); //Please enter the ip add>
 server.sin_family = AF_INET;
 server.sin_port = htons( 22);
 
@@ -23,6 +26,14 @@ if (connect(socket_desc , (struct sockaddr *)&server, sizeof(server)),0)
 puts("connect error");
 return 1;
 }
-puts("Connected");
+puts("Connected \n");
+//send some data
+message = "connect";
+if( send(socket_desc , message , strlen(message), 0)< 0)
+{
+puts("Send failed");
+return 1;
+}
+puts("Data Send\n");
 return 0;
 }
